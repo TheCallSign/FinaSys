5a5d5983af4b1814395dadc75fc4c6e106fcd604
stjohn <stjohn@SignZ>
2015-08-15 02:00:34 +0200

    Disabled output from the derby server.

--- src/finasys/ServerThread.java
+++ src/finasys/ServerThread.java
@@ -6,6 +6,8 @@
 package finasys;
 
 import java.io.PrintWriter;
+import java.io.Writer;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.derby.drda.NetworkServerControl;
 
@@ -16,7 +18,8 @@ import org.apache.derby.drda.NetworkServerControl;
 public class ServerThread extends Thread {
 
     private NetworkServerControl server;
-
+    public ServerThread(){
+    }
     public void shutdown() throws Exception {
         System.out.println("[Server Thread] Stopping Derby server.");
         server.shutdown();
@@ -27,9 +30,10 @@ public class ServerThread extends Thread {
         try {
             server = new NetworkServerControl();
             System.out.println("[Server Thread] Starting Derby server...");
-            server.start(new PrintWriter(System.out));
+            PrintWriter pw = new PrintWriter(System.out);
+            server.start(null);
         } catch (Exception ex) {
-            Logger.getLogger(FinaSys.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            Logger.getLogger(FinaSys.class.getName()).log(java.util.logging.Level.WARNING, null, ex);
         }
-    }
+    } 
 }
